cc64d252ff150c8820e6fa7cbdfe1581f380d825
Majid <majiddarvishnejad93@gmail.com>
2021-05-31 13:42:17 +0430

    feat: add presets

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 export function About(props: any) {
-  return <h1>About!</h1>;
+  return (
+    <div className='p-6 max-w-sm mx-auto bg-pink-100 rounded-xl shadow-md flex items-center space-x-4'>
+      <div>
+        <div className='text-xl font-medium text-black'>About</div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import * as React from 'react';
 export function Home(props: any) {
   return (
-    <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
+    <div className='p-6 max-w-sm mx-auto bg-green-100 rounded-xl shadow-md flex items-center space-x-4'>
       <div>
-        <div className='text-xl font-medium text-black'>ChitChat</div>
-        <p className='text-gray-500'>You have a new message!</p>
+        <div className='text-xl font-medium text-black'>Home</div>
       </div>
     </div>
   );
